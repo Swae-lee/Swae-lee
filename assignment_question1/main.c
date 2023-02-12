df98807c914b0e317647a3083f6cba0d24765875
SWALEH JAYDEN <swalehasnan@gmail.com>
2023-02-12 12:17:11 +0300

    Add files via upload

---
+++ assignment_question1/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+/*
+ * THE PROGRAM PRINTS THE ALPHABET IN LOWERCASE.
+ */
+    char alf = 97;
+    while (alf <= 122){
+        putchar(alf);
+        alf += 1;
+    }
+    return 0;
+}
